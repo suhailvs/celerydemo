cd651f2e645e5a803e4514bb8a51abfaca42bfa4
suhail <suhailvs@gmail.com>
2014-09-26 05:17:58 +0000

    some webfaction updates

--- ctest/wsgi.py
+++ ctest/wsgi.py
@@ -9,6 +9,11 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ctest.settings")
-
+virtenv =os.path.join(os.environ['HOME'],'webapps','djcelery','myenv','bin','activate_this.py')
+#virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtenv, dict(__file__=virtenv))
+except IOError:
+    pass
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
